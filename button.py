5b01ad848bb5936b1b3ddc0a6e672eceffa26334
AnthonyAnts <151478911+AnthonyAnts@users.noreply.github.com>
2026-05-11 01:43:02 +0800

    Update button.py

--- button.py
+++ button.py
@@ -1,29 +1,47 @@
 import pygame
 
 class Button():
-    def __init__(self, x, y, image, scale):
+    def __init__(self, x, y, image, scale=1, hitbox=None):
         width = image.get_width()
         height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+        self.base_image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.image = self.base_image.copy()
+        self.rect = self.image.get_rect(topleft=(x,y))
+        # dirty implementation to uh, keep our predefined buttons in place
+        self.draw_x = x
+        self.draw_y = y
         self.clicked = False
+
+        
+        if hitbox:
+            self.rect = pygame.Rect(hitbox)
+        else:
+            self.rect= self.image.get_rect(topleft=(x, y))
 
     def draw(self, surface):
         action = False
         #get mouse position
         pos = pygame.mouse.get_pos()
+        self.image = self.base_image.copy()
 
         #check mouseover and clicked conditions
         if self.rect.collidepoint(pos):
+            hover_image = self.base_image.copy()
+            hover_image.set_alpha(180)
+            self.image = hover_image
+
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
                 action = True
         
+        else:
+            self.image.set_alpha(255)
+        
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+        surface.blit(self.image, (self.draw_x, self.draw_y))
+        # pygame.draw.rect(surface, (255, 0, 0), self.rect, 2) # <-- DEBUG FOR HITBOX
 
         return action
      #38 x 58
